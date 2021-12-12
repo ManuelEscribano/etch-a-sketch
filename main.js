58dd003afacfb2f9f4e42edcbab14c6920576571
ManuelEscribano <manuel.escribano@gmx.de>
2021-12-12 04:12:52 -0300

    Add comments to createGrid function

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ const gridContainer = document.querySelector(".grid-container");
 
 // SECTION Variables
 
+// Create grid with a default size of 16 x 16
 const createGrid = function (gridSize = 16) {
   let gridElement = document.createElement("div");
   gridElement.className = "grid-element";
   gridContainer.appendChild(gridElement);
+
+  // Create row element, and add "gridSize"
   for (let h = 1; h <= gridSize; h++) {
     let rowElement = document.createElement("div");
     rowElement.className = "row";
     rowElement.style.height = `${700 / gridSize}px`;
+
+    // Create box element and append it to row element.
     for (let i = 1; i <= gridSize; i++) {
       let boxElement = document.createElement("div");
       boxElement.style.height = `${700 / gridSize}px`;
@@ -18,6 +23,8 @@ const createGrid = function (gridSize = 16) {
       boxElement.className = "box";
       rowElement.appendChild(boxElement);
     }
+
+    // Append rowElement to div.grid-element
     gridElement.appendChild(rowElement);
   }
 };
